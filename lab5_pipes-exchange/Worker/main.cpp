90b0bf6ad7d42ce6664d4035cbdbca4fbc7852e1
Dmitry <dimafamcs@gmail.com>
2025-12-24 09:29:05 +0300

    Implement named-pipe protocol: Task/Result exchange

--- lab5_pipes-exchange/Worker/main.cpp
+++ lab5_pipes-exchange/Worker/main.cpp
@@ -1,6 +1,6 @@
 #include <windows.h>
 #include <iostream>
-#include <string>
+#include <algorithm>
 #include "protocol.h"
 using namespace std;
 
@@ -9,32 +9,41 @@ string MakePipeOut(int id) { return "\\\\.\\pipe\\worker_out_" + to_string(id); 
 
 int main(int argc, char* argv[]) {
 
-    if (argc < 2) {
-        cout << "Worker started without ID.\n";
-        return 1;
+    int id = atoi(argv[1]);
+    cout << "Worker " << id << " started.\n";
+
+    HANDLE hIn = CreateFileA(MakePipeIn(id).c_str(), GENERIC_READ, 0, NULL, OPEN_EXISTING, 0, NULL);
+    HANDLE hOut = CreateFileA(MakePipeOut(id).c_str(), GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
+
+    while (true) {
+
+        Task task{};
+        DWORD readBytes = 0;
+
+        BOOL ok = ReadFile(hIn, &task, sizeof(Task), &readBytes, NULL);
+        if (!ok || readBytes == 0) continue;
+
+        // --- Exit command ---
+        if (task.type == TASK_EXIT) {
+            cout << "Worker " << id << " exiting...\n";
+            break;
+        }
+
+        // --- Processing (simple: sort array) ---
+        Result res{};
+        res.code = 0;
+        res.size = task.size;
+
+        for (int i = 0; i < task.size; i++)
+            res.data[i] = task.data[i];
+
+        sort(res.data, res.data + res.size);
+
+        DWORD written;
+        WriteFile(hOut, &res, sizeof(Result), &written, NULL);
     }
 
-    int id = atoi(argv[1]);
-    cout << "Worker " << id << " starting...\n";
-
-    HANDLE hIn = CreateFileA(
-        MakePipeIn(id).c_str(),
-        GENERIC_READ,
-        0, NULL,
-        OPEN_EXISTING,
-        0, NULL
-    );
-
-    HANDLE hOut = CreateFileA(
-        MakePipeOut(id).c_str(),
-        GENERIC_WRITE,
-        0, NULL,
-        OPEN_EXISTING,
-        0, NULL
-    );
-
-    cout << "Worker " << id << ": connected to pipes.\n";
-
-    cout << "Commit 1: Worker ready.\n";
+    CloseHandle(hIn);
+    CloseHandle(hOut);
     return 0;
 }
